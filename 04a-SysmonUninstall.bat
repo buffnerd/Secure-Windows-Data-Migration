4f13ccb9106f8acb5ae90d02e32dd17ef7b8268f
Buff Nerd <94365548+buffnerd@users.noreply.github.com>
2023-01-08 18:15:16 -0600

    Add files via upload
    This is a batch file that will uninstall sysmon from all folders including temp where "Sysmonbackup" is whatever the organizations backup name of Sysmon is

---
+++ 04a-SysmonUninstall.bat
@@ -0,0 +1,7 @@
+%Windir%\Sysmon.exe -u
+%Windir%\Sysmonbackup.exe -u
+%Windir%\CcmTemp\Sysmonbackup.exe -u
+Del %Windir%\Sysmon.exe -u
+Del %Windir%\Sysmonbackup.exe -u
+Del %Windir%\CcmTemp\Sysmonbackup.exe -u
+pause
